1f3a146ce126b17656a133f371acb52a7f4dcc97
PatrickS137 <101444842+PatrickS137@users.noreply.github.com>
2022-10-03 01:11:18 -0500

    a

--- IdeaProjects/Unit1/src/Unit2/notes/Chevy.java
+++ IdeaProjects/Unit1/src/Unit2/notes/Chevy.java
@@ -74,7 +74,7 @@ public class Chevy {
         finalOutput += ("\tBASE PRICE:\t\t\t\t\t$" + basePrice);
         finalOutput += ("\n\tMILES:\t\t\t\t\t\t" + this.mileage);
         finalOutput += ("\n\tFUEL EFFICIENCY:\t\t\t" + this.fuelEfficiency + "mpg");
-        grandTotal = TAXRATE * (upgradePrice);
+        grandTotal = TAXRATE * (upgradePrice + basePrice);
         int test = 0;
         if (luxuryPackage == true) {
             finalOutput += ("\n\t - Luxury Package");
