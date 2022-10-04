cc28c47a163793a96600a7db4c93def4129dad90
PatrickS137 <101444842+PatrickS137@users.noreply.github.com>
2022-10-04 10:09:49 -0500

    new notes

---
+++ IdeaProjects/Unit1/src/Unit4/notes/loops.java
@@ -0,0 +1,23 @@
+package Unit4.notes;
+
+import java.util.Scanner;
+public class loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+        // user will enter -999 to exit
+        //need int vars for sum and count
+        // prompt the user for their value
+        //while a loop making sure they want to continue
+        //add to the sum
+        sum += input.nextInt();
+        //increment the count
+        
+        //calculate the average
+        return(sum/count)
+        //print
+
+    }
+}
