a05941e8753eaf298b3fa07e7b3a52f473ec5800
PatrickS137 <101444842+PatrickS137@users.noreply.github.com>
2022-12-08 10:10:01 -0600

    12/8

--- IdeaProjects/Unit1/src/Unit6/arrays/Question.java
+++ IdeaProjects/Unit1/src/Unit6/arrays/Question.java
@@ -9,11 +9,17 @@ public class Question {
     private String answerChoices;
     private String correctAnswer;
     private int pointValue;
+    private int question;
 
-    public Question(String text, String answerChoices, String correctAnswer, int pointValue) {
+    public Question(String text, String answerChoices, String correctAnswer, int pointValue, int question) {
         this.text = text;
         this.answerChoices = answerChoices;
         this.correctAnswer = correctAnswer;
         this.pointValue = pointValue;
+        this.question = question;
+    }
+
+    public String toString(){
+        return (question + ")  " + text + "\n" + answerChoices);
     }
 }
--- IdeaProjects/Unit1/src/Unit6/arrays/TriviaGame.java
+++ IdeaProjects/Unit1/src/Unit6/arrays/TriviaGame.java
@@ -1,5 +1,25 @@
 package Unit6.arrays;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TriviaGame {
     //point values are randomized lunch cost(2.5 to 3.25), usd to indonesian dollar(rupiah)
+    private String name;
+    private int currentPoints;
+    private Question[] questionArray;
+
+    File questions = new File ("mcQuestions.txt");
+    Scanner inQ = new Scanner(questions);
+
+    public TriviaGame() throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        
+    }
+
+
+
 }
+
