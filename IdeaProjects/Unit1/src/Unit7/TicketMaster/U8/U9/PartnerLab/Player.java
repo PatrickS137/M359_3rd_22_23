caf8783f65f8f15517c59a8487ce0ebcb6610d38
PatrickS137 <101444842+PatrickS137@users.noreply.github.com>
2023-03-06 10:09:23 -0600

    ijnnSGjhnGSRDjnh

---
+++ IdeaProjects/Unit1/src/Unit7/TicketMaster/U8/U9/PartnerLab/Player.java
@@ -0,0 +1,4 @@
+package Unit7.TicketMaster.U8.U9.PartnerLab;
+
+public class Player {
+}
---
+++ IdeaProjects/Unit1/src/Unit7/TicketMaster/U8/U9/PartnerLab/Three_Pt_Shooter.java
@@ -0,0 +1,4 @@
+package Unit7.TicketMaster.U8.U9.PartnerLab;
+
+public class Three_Pt_Shooter extends Player{
+}
